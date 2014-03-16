44774be8d93fa05f2e93440a129a34266c3c8550
E1even <henry_ge@outlook.com>
2014-03-16 22:10:37 +0800

    add 720P solution

--- js/script.js
+++ js/script.js
@@ -54,8 +54,8 @@ $(document).ready(function(){
         $(".parallax_wrap").animate({top:'0'},"linear");
         $(".parallax_background").animate({top:'-200%'},"linear");
         $(".parallax_btns").removeClass("rotate120 rotate240 rotate360").addClass("rotate360");
-        $(".parallax_content").animate({left:'100rem'},100);
-        $(".parallax_content1").animate({left:'40rem'},1000);
+        $(".parallax_content").animate({left:'150%'},100);
+        $(".parallax_content1").animate({left:'60%'},1000);
     });
     $(".parallax_btn2").click(function(){
         $(".parallax_wrap").animate({top:'-100%'},"linear");
@@ -63,8 +63,8 @@ $(document).ready(function(){
         $(".parallax_btns").removeClass("rotate120 rotate240 rotate360").addClass("rotate120");
         $(".parallax_content").removeClass("parallax_content_show");
         $(".parallax_content2").addClass("parallax_content_show");
-        $(".parallax_content").animate({left:'100rem'},100);
-        $(".parallax_content2").animate({left:'40rem'},1000);
+        $(".parallax_content").animate({left:'150%'},100);
+        $(".parallax_content2").animate({left:'60%'},1000);
     });
     $(".parallax_btn3").click(function(){
         $(".parallax_wrap").animate({top:'-200%'},"linear");
@@ -72,8 +72,8 @@ $(document).ready(function(){
         $(".parallax_btns").removeClass("rotate120 rotate240 rotate360").addClass("rotate240");
         $(".parallax_content").removeClass("parallax_content_show");
         $(".parallax_content3").addClass("parallax_content_show");
-        $(".parallax_content").animate({left:'100rem'},100);
-        $(".parallax_content3").animate({left:'40rem'},1000);
+        $(".parallax_content").animate({left:'150%'},100);
+        $(".parallax_content3").animate({left:'60%'},1000);
     });
 
 });
