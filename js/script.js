ce2f153f9d6e5afab957d8e572db831a80920e88
E1even <henry_ge@outlook.com>
2014-03-20 19:05:36 +0800

    load test

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@
  */
 $(document).ready(function(){
 
-    $(document).onload(function(){
+    $(window).load(function(){
         $(".bar_in").animate({width:'100%'},11000,"linear");
         $("#btn_enter").css("display","block").addClass("btn_down");
-    });
-    
+    })
+
+
+
     //initial pages
     $(".page_middle").nextAll().addClass("page_bottom");
     $(".page_middle").prevAll().addClass("page_top");
